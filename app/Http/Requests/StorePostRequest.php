f4ad3234ecadb8117b0838f9285150b9db64e286
Adeyemi Joshua <joshuaadeyemi445@gmail.com>
2024-08-22 13:09:27 +0100

    worked on categories

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -29,7 +29,6 @@ class StorePostRequest extends FormRequest
             // "post_pdf_path" => "nullable|mimes:pdf,doc,docx",
             // "post_song_path" => "nullable|mimes:mp3,wav,aac,flac",
             'category' => 'required|array',
-            'category.*' => 'string|max:255',
             'post_title' => 'required|string|max:255',
             'PostbodyHtml' => 'required',
             'postbodyJson' => 'nullable',
